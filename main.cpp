bf76704f96f9f62b704e9dd17a4c6877c9bde317
SohamDoshi15 <soham1518@gmail.com>
2025-03-26 22:15:38 +0530

    The previous code in full was super glitchy so modified the entire code

--- main.cpp
+++ main.cpp
@@ -1,136 +1,164 @@
 #include <iostream>
 #include <vector>
-#include <string>
-using namespace std;
 #include <windows.h>
-#include <thread>
-const int nFieldWidth = 10;
-const int nFieldHeight = 20;
-class Tetromino{
-    private:
-    wstring pieceShape;
-    int pieceId;
-    int positionX;
-    int positionY;
-    int currentRotation;
+#include <conio.h>
+#include <ctime>
+#include <cstdlib>
+#include <fstream>
+#include <algorithm>
 
-    int calculateRotation(int blockX,int blockY){
-        int rotatedIndex=0;
-        //currentRotation is 0,90,180 and 270 degrees
-        if(currentRotation%4==0){
-            rotatedIndex=blockY*4+blockX;
-        }
-        else if(currentRotation%4==1){
-            rotatedIndex=12+blockY-(blockX*4);
-        }
-        else if(currentRotation%4==2){
-            rotatedIndex=15-(blockY*4)-blockX;
-        }
-        else if(currentRotation%4==3){
-            rotatedIndex=3-blockY+(blockX*4);
-        }
-        return rotatedIndex;
+using namespace std;
+
+// Console manipulation functions (Windows-specific)
+void SetConsoleCursorPositionXY(int x, int y) {
+    COORD coord = { static_cast<SHORT>(x), static_cast<SHORT>(y) };
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+
+void ShowConsoleCursor(bool showFlag) {
+    HANDLE out = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_CURSOR_INFO cursorInfo;
+    GetConsoleCursorInfo(out, &cursorInfo);
+    cursorInfo.bVisible = showFlag;
+    SetConsoleCursorInfo(out, &cursorInfo);
+}
+
+// Constants
+const int BOARD_WIDTH = 10;
+const int BOARD_HEIGHT = 20;
+const int BLOCK_SIZE = 2; // For visual representation (2x2 block in console)
+const string HIGH_SCORE_FILE = "highscore.txt";
+
+// Tetromino shapes (I, O, T, S, Z, J, L)
+const vector<vector<vector<int>>> SHAPES = {
+    {{1, 1, 1, 1}}, // I
+    {{1, 1}, {1, 1}}, // O
+    {{0, 1, 0}, {1, 1, 1}}, // T
+    {{0, 1, 1}, {1, 1, 0}}, // S
+    {{1, 1, 0}, {0, 1, 1}}, // Z
+    {{1, 0, 0}, {1, 1, 1}}, // J
+    {{0, 0, 1}, {1, 1, 1}}  // L
+};
+
+// Colors for different tetrominoes
+const int COLORS[] = { 9, 14, 13, 10, 12, 11, 15 }; // Blue, Yellow, Magenta, Green, Red, Cyan, White
+
+class Tetromino {
+public:
+    vector<vector<int>> shape;
+    int x, y; // Position on the board
+    int color;
+
+    Tetromino(int type) {
+        shape = SHAPES[type];
+        color = COLORS[type];
+        x = BOARD_WIDTH / 2 - shape[0].size() / 2;
+        y = 0; // Start at the top
     }
-    public:
-    Tetromino(wstring shapeInput, int idInput){
-        pieceShape=shapeInput;
-        pieceId=idInput;
-        positionX=nFieldWidth/2;
-        positionY=0;
-        currentRotation=0;
+
+    void rotate() {
+        int rows = shape.size();
+        int cols = shape[0].size();
+        vector<vector<int>> rotated(cols, vector<int>(rows));
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                rotated[j][rows - 1 - i] = shape[i][j];
+            }
+        }
+        shape = rotated;
     }
-    bool canFit(int newX,int newY, int newRotation,unsigned char gameField[][nFieldWidth]){
-        currentRotation=newRotation;
-        for(int blockX=0;blockX<4;blockX++){
-            for (int blockY = 0; blockY< 4; blockY++){
-                int shapeIndex=calculateRotation(blockX,blockY);
-                int fieldX=newX+blockX;
-                int fieldY=newY+blockY;
-                if (fieldX >= 0 && fieldX < nFieldWidth){
-                    if(fieldY>=0 && fieldY < nFieldHeight){
-                        if(pieceShape[shapeIndex] != L'.' && gameField[fieldY][fieldX] != 0){
-                            return false;
-                        }
+};
+
+class Board {
+public:
+    vector<vector<int>> grid; // 0 = empty, >0 = occupied (color value)
+    Tetromino* currentTetromino;
+
+    Board() : grid(BOARD_HEIGHT, vector<int>(BOARD_WIDTH, 0)), currentTetromino(nullptr) {}
+
+    ~Board() {
+        if (currentTetromino) {
+            delete currentTetromino;
+            currentTetromino = nullptr;
+        }
+    }
+
+    bool isCollision(int newX, int newY, const vector<vector<int>>& shape) const {
+        for (int i = 0; i < shape.size(); i++) {
+            for (int j = 0; j < shape[i].size(); j++) {
+                if (shape[i][j]) {
+                    int boardX = newX + j;
+                    int boardY = newY + i;
+                    if (boardX < 0 || boardX >= BOARD_WIDTH || boardY >= BOARD_HEIGHT ||
+                        (boardY >= 0 && grid[boardY][boardX] > 0)) {
+                        return true;
                     }
                 }
             }
         }
-        currentRotation=newRotation;
-        return true;
+        return false;
     }
-    wstring getPieceShape(){
-        return pieceShape;
-    }
-    int getPieceId(){
-        return pieceId;
-    }
-    int getPositionX(){
-        return positionX;
-    }
-    int getPositionY(){
-        return positionY;
-    }
-    int getCurrentRotation(){
-        return currentRotation;
-    }
-    void setPosition(int newX, int newY){
-        positionX=newX;
-        positionY=newY;
-    }
-    void move(int changeX,int changeY){
-        positionX=positionX+changeX;
-        positionY=positionY+changeY;
-    }
-    void setRotation(int newRotation){
-        currentRotation=newRotation;
-    }
-};
 
-int nScreenWidth = 80;
-int nScreenHeight = 30;
-Tetromino structuredTetrominos[7];
-unsigned char pField[nFieldHeight][nFieldWidth];
-
-int main(){
-    wchar_t *screen=new wchar_t[nScreenWidth*nScreenHeight];
-    for(int i=0 ; i < nScreenWidth*nScreenHeight; i++){
-        screen[i]=L' ';
-    }
-    HANDLE hConsole=CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0 , NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
-    SetConsoleActiveScreenBuffer(hConsole);
-    DWORD dwBytesWritten=0;
-
-    structuredTetrominos[0] = Tetromino(L"..X...X...X...X.", 0); // I
-    structuredTetrominos[1] = Tetromino(L"..X..XX...X.....", 1); // J
-    structuredTetrominos[2] = Tetromino(L".....XX..XX.....", 2); // O
-    structuredTetrominos[3] = Tetromino(L"..X..XX..X......", 3); // L
-    structuredTetrominos[4] = Tetromino(L".X...XX...X.....", 4); // S
-    structuredTetrominos[5] = Tetromino(L".X...X...XX.....", 5); // T
-    structuredTetrominos[6] = Tetromino(L"..X...X..XX.....", 6); // Z
-
-    for(int x=0; x<nFieldWidth ; x++ ){
-        for(int y=0; y<nFieldHeight ; y++){
-            if(x==0 || x== nFieldWidth-1 || y==nFieldHeight-1){
-                pField[y][x]=9; //sets borders to 9
-            }
-            else{
-                pField[y][x]=0; //sets inner area to 0
+    void mergeTetromino() {
+        if (!currentTetromino) return;
+        for (int i = 0; i < currentTetromino->shape.size(); i++) {
+            for (int j = 0; j < currentTetromino->shape[i].size(); j++) {
+                if (currentTetromino->shape[i][j]) {
+                    int boardX = currentTetromino->x + j;
+                    int boardY = currentTetromino->y + i;
+                    if (boardY >= 0) {
+                        grid[boardY][boardX] = currentTetromino->color;
+                    }
+                }
             }
         }
     }
 
-    bool bKey[5];
-    int nCurrentPiece=0;
-    int nSpeed=20;
-    int nSpeedCount=0;
-    bool bForceDown=false;
-    bool bRotateHold=true;
-    bool bHardDropHold=true;
-    int nPieceCount=0;
-    int nScore=0;
-    vector <int> vLines;
-    bool bGameOver=false;
+    int clearLines() {
+        int linesCleared = 0;
+        for (int i = BOARD_HEIGHT - 1; i >= 0; i--) {
+            bool full = true;
+            for (int j = 0; j < BOARD_WIDTH; j++) {
+                if (grid[i][j] == 0) {
+                    full = false;
+                    break;
+                }
+            }
+            if (full) {
+                linesCleared++;
+                grid.erase(grid.begin() + i);
+                grid.insert(grid.begin(), vector<int>(BOARD_WIDTH, 0));
+            }
+        }
+        return linesCleared;
+    }
 
+    bool isGameOver() const {
+        if (!currentTetromino) return false;
+        return isCollision(currentTetromino->x, currentTetromino->y, currentTetromino->shape);
+    }
+};
 
-    
-}
+class Game {
+private:
+    Board board;
+    long long score;
+    long long highScore;
+    bool gameOver;
+    bool paused;
+    int fallDelayMs;
+    const int BASE_FALL_DELAY_MS = 500;
+    Tetromino* nextTetromino;
+    vector<vector<int>> lastDisplayGrid;
+    bool firstMove;
+
+    void loadHighScore() {
+        ifstream file(HIGH_SCORE_FILE);
+        if (file.is_open()) {
+            file >> highScore;
+            file.close();
+        } else {
+            highScore = 0;
+            cerr << "Warning: Could not load high score file." << endl;
+        }
+    }
